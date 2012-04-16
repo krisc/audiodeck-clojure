0cc8247b67d06a56be9343db1dc7fc2c2b333e25
Kris Calabio <kriscalabio@gmail.com>
2012-04-16 14:05:29 -0700

    Global playlist is now a deck object.  Multiple decks can be created, but will not work properly yet.  So stick to just one deck for now.

--- src/player/core.clj
+++ src/player/core.clj
@@ -2,96 +2,106 @@
   (:import (java.io IOException)
            (org.puredata.core PdBase)))
 
-(def audioThread (new JavaSoundThread 44100 2 16))
-(def patch (atom 0))
+;; deck object
+(defn deck []
+  (let [playlist (atom []) current-pos (atom 0)
+        playing (atom false) patch (atom 0)
+        audioThread (new JavaSoundThread 44100 2 16)]
 
-;; Player state variables
-(def playing (atom false))
-(def playlist (atom []))
-(def current-pos (atom 0))
+    (defn open-patch []
+      (reset! patch (PdBase/openPatch "src/pd/deck.pd"))
+      (.start audioThread))
 
-;;Internal functions
-(defn playing? []
-  (if (= @playing true)
-    true
-    false))
+    (defn close-patch []
+      (.interrupt audioThread)
+      (.join audioThread)
+      (PdBase/closePatch @patch))
 
-(defn open-patch []
-  (reset! patch (PdBase/openPatch "src/pd/deck.pd"))
-  (.start audioThread)
-  )
+    ;; change to add a list of files
+    (defn add! [files]
+      (swap! playlist conj files))
 
-(defn close-patch []
-  (.interrupt audioThread)
-  (.join audioThread)
-  (PdBase/closePatch @patch))
+    (defn load-clip [filename]
+      (println "loading clip...")
+      (PdBase/sendBang "ctl-stop")
+      (if (= (PdBase/sendSymbol  "ctl-loadclip" filename) 0)
+        (println "Clip loaded")
+        (println "Error while loading clip")))
 
+    (defn ctl-stop []
+      (reset! playing false)
+      (if (= (PdBase/sendBang  "ctl-stop") 0)
+        (println "Stopped")
+        (println "Error while stopping")))
 
-;;Player Controls
+    ;; change boolean logic here!
+    (defn ctl-play []
+      (println "playing")
+      (if (= @playing false)
+        (do (load-clip (get @playlist @current-pos))
+            (reset! playing true)))
+      (PdBase/sendBang "ctl-play"))
 
-(defn load-clip [filename]
-  (println "loading clip...")
-  (PdBase/sendBang "ctl-stop")
-  (if (= (PdBase/sendSymbol  "ctl-loadclip" filename) 0)
-    (println "Clip loaded")
-    (println "Error while loading clip")))
+    (defn ctl-pause []
+      (PdBase/sendBang "ctl-pause"))
 
-(defn ctl-stop []
-  (reset! playing false)
-  (if (= (PdBase/sendBang  "ctl-stop") 0)
-    (println "Stopped")
-    (println "Error while stopping")))
+    (defn ctl-resume []
+      (PdBase/sendBang "ctl-resume"))
 
-;; change boolean logic here!
-(defn ctl-play []
-  (println "playing")
-  (if (= @playing false)
-    (do (load-clip (get @playlist @current-pos))
-        (reset! playing true)))
-  (PdBase/sendBang "ctl-play"))
+    (defn print-current-pos []
+      (println (str "Current Position: " @current-pos)))
 
-(defn ctl-pause []
-  (PdBase/sendBang "ctl-pause"))
+    (defn print-playlist []
+      (println @playlist))
 
+    (defn load-new-pos []
+      (if (= @playing true)
+        (do
+          (load-clip (get @playlist @current-pos))
+          (ctl-play))
+        (load-clip (get @playlist @current-pos)))
+      (print-current-pos))
 
-;; Playlist Controls
-
-(defn add [filename]
-  (swap! playlist conj filename))
-
-(defn print-current-pos []
-  (println (str "Current Position: " @current-pos)))
-
-(defn print-playlist []
-  (println @playlist))
-
-(defn load-new-pos []
-  (if (= @playing true)
-    (do
-      (load-clip (get @playlist @current-pos))
-      (ctl-play))
-    (load-clip (get @playlist @current-pos)))
-  (print-current-pos))
-
-(defn ctl-seek [fun]
-  (swap! current-pos fun 1)
-  (load-new-pos))
-
-(defn ctl-next []
-  (if (= (inc @current-pos) (count @playlist))
-    (do
-      (reset! current-pos 0)
+    (defn ctl-seek! [fun]
+      (swap! current-pos fun 1)
       (load-new-pos))
-    (ctl-seek +)))
 
-(defn ctl-prev []
-  (if (< (dec @current-pos) 0)
-    (do
-      (reset! current-pos (dec (count @playlist)))
-      (load-new-pos))
-    (ctl-seek -)))
+    (defn ctl-next! []
+      (if (= (inc @current-pos) (count @playlist))
+        (do
+          (reset! current-pos 0)
+          (load-new-pos))
+        (ctl-seek! +)))
+
+    (defn ctl-prev! []
+      (if (< (dec @current-pos) 0)
+        (do
+          (reset! current-pos (dec (count @playlist)))
+          (load-new-pos))
+        (ctl-seek! -)))
+
+    (defn playing? []
+      (if (= @playing true)
+        true
+        false))
+
+    (defn dispatch [m & args]
+      (cond
+        (= m 'add!) (add! (first args))
+        (= m 'pos) (print-current-pos)
+        (= m 'list) (print-playlist)
+        (= m 'play) (ctl-play)
+        (= m 'stop) (ctl-stop)
+        (= m 'resume) (ctl-resume)
+        (= m 'pause) (ctl-pause)
+        (= m 'next!) (ctl-next!)
+        (= m 'prev!) (ctl-prev!)
+        (= m 'playing?) (playing?)
+        :else (println "Error: Unknown operation -- deck")))
+
+    (open-patch)
+    dispatch))
 
 (defn -main [& args]
-  (open-patch)
-  (Thread/sleep 5000)
-  (close-patch))
+  (def deck0 (deck))
+  (println "New deck: deck0"))
